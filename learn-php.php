4693adb7f32ac3ab111f62372bd42a11c2cf04e0
Hemaet33 <hemaet3333@gmail.com>
2023-11-06 20:42:00 +0600

    changes

--- learn-php.php
+++ learn-php.php
@@ -1,15 +1,15 @@
 <?php
 $string = "hello, World";
-      echo str_replace('World','Riyad',$string);
-      echo "<br>";
-      echo chr(65);
-      echo "<br>";
-      $textArr = explode(',',$string);
-      print_r($textArr);
-      echo "<br>";
-      echo join(" ",$textArr);
+echo str_replace('World','Riyad',$string);
 echo "<br>";
-      echo lcfirst($string);
+echo chr(65);
+echo "<br>";
+$textArr = explode(',',$string);
+print_r($textArr);
+echo "<br>";
+echo join(" ",$textArr);
+echo "<br>";
+echo lcfirst($string);
 echo "<br>";
 echo ucfirst($string);
 echo "<br>";
@@ -17,20 +17,20 @@ echo strpos($string,"World");
 echo "<br>";
 echo str_word_count("Hello world Planet earth");
 // PHP Class
-    class Car{
-      public $model;
-      public $color;
-      public function __construct ($model,$color){
-      $this->model=$model;
-      $this->color=$color;
-      }
-      public function display(){
-        $message = "My car is of model $this->model & color of the car is $this->color.";
-        return $message;
-      }
-    }
+class Car{
+public $model;
+public $color;
+public function __construct ($model,$color){
+$this->model=$model;
+$this->color=$color;
+}
+public function display(){
+  $message = "My car is of model $this->model & color of the car is $this->color.";
+  return $message;
+}
+}
 
-    $car = new Car('Toyota','white');
-    echo $car->display();
+$car = new Car('Toyota','white');
+echo $car->display();
 
-    ?>
+?>
